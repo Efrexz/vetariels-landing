120ed7b9251a177d0115039b2736ab67a83ff67d
Efrexz <zyzz_448@hotmail.com>
2025-06-14 21:19:31 -0500

    creando header y archivos iniciales del proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": "/src/components",
+        "@assets": "/src/assets",
+        "@layouts": "/src/layouts",
+      },
+    },
+  },
+});
